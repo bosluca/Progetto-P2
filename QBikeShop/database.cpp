5d6529797712b6d737df624d6db5b52fe2d3faf7
Luca Boscolo <bosluca93@gamil.com>
2017-03-09 17:20:41 +0100

    Agg
    Agg

--- QBikeShop/database.cpp
+++ QBikeShop/database.cpp
@@ -2,4 +2,35 @@
 //QString utentidatabase::fileutenti = "../Progetto/utentiDb.xml";
 //QString articolidatabase::filearticoli = "../Progetto/articoliDb.xml";
 
+utentidatabase::utentidatabase(){
+    Load();
+}
+utentidatabase::~utentidatabase(){
+    Close();
+    contenitore::iteratore it=usDB.begin();
+    for(;it!=usDB.end();++it){
+        QString tmp=it.getUtentiIT()->getUsUname();
+        rimuoviUtente(tmp);
+    }
+}
+
+void utentidatabase::togli(const QString &uname){
+    contenitore::iteratore it=usDB.begin();
+    for(;it!=usDB.end();++it){
+        if(it.getUtentiIT()->getUsUname()==uname)
+            usDB.RimUtente(*it);
+    }
+}
+
+void utentidatabase::rimuoviUtente(const QString &uname){
+    togli(uname);
+}
+
+contenitore::iteratore utentidatabase::begin(){return usDB.begin();}
+contenitore::iteratore utentidatabase::end(){return usDB.end();}
+
+void utentidatabase::Load(){
+
+}
+
 
