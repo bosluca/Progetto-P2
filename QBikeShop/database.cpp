a5f7b91994202f9c762ad0440a27c42037a902e6
Luca Boscolo <bosluca93@gamil.com>
2017-03-11 10:34:00 +0100

    data
    data

--- QBikeShop/database.cpp
+++ QBikeShop/database.cpp
@@ -5,6 +5,7 @@
 utentidatabase::utentidatabase(){
     Load();
 }
+
 utentidatabase::~utentidatabase(){
     Close();
     contenitore::iteratore it=usDB.begin();
@@ -30,7 +31,31 @@ contenitore::iteratore utentidatabase::begin(){return usDB.begin();}
 contenitore::iteratore utentidatabase::end(){return usDB.end();}
 
 void utentidatabase::Load(){
+    QString username="provv";
+    username* u=0;
+    QString classe="base";
+    QString n=0; //nome
+    QString c=0; //cognome
+    QString i=0; //indirizzo
+    QString pw=0; //password
+    QString dataNascita=0;
+    datiCarta* dc=0;
+    //QString t;
+    bool prem=false;
 
+    QString idArt=0;
+    QString nA=0; //nome articolo
+    int prz=0;
+    list<articolo*> acq;
+
+    QFile qfile(fileUser);
+    qfile.open(QFile::ReadWrite);
+    qfile.close();
+
+    if(!qfile.open(QFile::ReadOnly | QFile::Text))
+        std::cout<<" Errore con i file " << std::endl;
+
+    QXmlStreamReader xmlReader(&qfile);
 }
 
 
